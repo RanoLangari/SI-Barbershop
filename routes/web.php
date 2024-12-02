811742371231e1d894c44296a65a7b33e274034c
RanoLangari <langarirano@gmail.com>
2024-12-03 01:57:05 +0800

    feat: initialize controller & routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\BarbermanController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\LayananController;
+use App\Http\Controllers\Admin\ReservasiController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
+//Public Routes
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'loginAll'])->name('login');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'registerPelanggan'])->name('register');
+
+// Admin Routes
